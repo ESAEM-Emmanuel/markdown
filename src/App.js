b707ce6ee01017c2474861f8fa5778024ffe4056
ESAEM-Emmanuel <emmanuelessimagel@gmail.com>
2024-09-06 11:59:00 +0100

    finalisation markdown

--- src/App.js
+++ src/App.js
@@ -1,30 +1,64 @@
 import './App.css';
 import { Component, Fragment } from 'react';
 import { sampleText } from './sampleText';
+import { marked, Marked } from 'marked';
 
 
-function App() {
-  return (
-    <Fragment>
-      
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-6">
-            <textarea
-            value={sampleText} 
-            className = "form-control"
-            rows="35"/ >
-          </div> 
-          <div className="col-sm-6">
-            <div> 
-              {sampleText} 
-            </div>
-          </div> 
-        </div>
-      </div> 
-    </Fragment>
+class App extends Component {
+  state = {
+    text : sampleText
+  }
+  // methodes utilisée uniquement dans les composants de typle class
+  componentDidMount(){
+    const text = localStorage.getItem('text')
+    if (text){
+      this.setState({text})
 
-  );
+    }else{
+      this.setState({text : sampleText})
+    }
+    console.log('je suis monté!');
+  }
+  componentDidUpdate(){
+    const {text} = this.state
+    localStorage.setItem('text', text)
+  }
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({text})
+  }
+  // sanitized est pour ne pas interpreter les balise html
+  renderText = (text) => {
+    const __html = marked(text, {sanitize: true})
+    return {__html}
+  }
+  render(){
+    return (
+      <Fragment>
+        
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <textarea
+              onChange={this.handleChange}
+              value={this.state.text} 
+              className = "form-control"
+              rows="35">
+              </textarea> 
+            </div> 
+            <div className="col-sm-6">
+                {/* Gestion de l'affichage du texte potentiellement dangereux  */}
+              <div dangerouslySetInnerHTML={this.renderText(this.state.text) }>
+
+              </div>
+            </div> 
+          </div>
+        </div> 
+      </Fragment>
+  
+    );
+  }
+  
 }
 
 export default App;
